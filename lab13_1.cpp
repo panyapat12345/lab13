c6e9a7e79e6cda863af067e04aae72de88c89c75
Panyapat Wongdee <panyapat12345@gmail.com>
2021-12-24 15:43:52 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int y){
+long long int fibonacci(long long int y){
     if(y == 0)          return 0;
     else if(y == 1)     return 1;
     else                return fibonacci(y-1) + fibonacci(y-2);
+    
 }
