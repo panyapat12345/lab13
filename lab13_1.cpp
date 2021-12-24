58440ebbea9e4b69a943455fcad1023b9bfc3e4a
Panyapat Wongdee <panyapat12345@gmail.com>
2021-12-24 15:43:15 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int y){
+    if(y == 0)          return 0;
+    else if(y == 1)     return 1;
+    else                return fibonacci(y-1) + fibonacci(y-2);
 }
